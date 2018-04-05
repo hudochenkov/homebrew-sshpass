5767a109b5bfa969ee5664595e8640fce04898a0
forkbomber <forkbomber@users.noreply.github.com>
2018-04-05 08:25:24 +0000

    sshpass: add support to build trunk branch from svn

--- sshpass.rb
+++ sshpass.rb
@@ -2,8 +2,15 @@ class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
   sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
+  head do
+    url "https://svn.code.sf.net/p/sshpass/code/trunk"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
 
   def install
+    system "./bootstrap" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
@@ -12,6 +19,6 @@ class Sshpass < Formula
   end
 
   test do
-    system "sshpass"
+    system "#{bin}/sshpass"
   end
 end
